95ae41b692ec6beb8df13067ea1cd2cec8353e3a
unknown <once6474@naver.com>
2026-03-09 19:42:54 +0900

    update

---
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+    @GetMapping("/profile")
+    public Profile profile() {
+        return new Profile("홍길동", 20);
+    }
+
+    record Profile(String name, int age) {}
+}
